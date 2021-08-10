c322d6fc2c6e768723f43cf0cbc9110398d22a9e
Raphael <raphaelfreire.dias@gmail.com>
2021-08-10 16:21:45 -0300

    Controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,18 @@ import { CreateUserController } from "./controller/CreateUsersController";
 import { CreateTagController } from "./controller/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
+import { CreateComplimentController } from "./controller/CreateComplimentController";
+
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/compliment", createComplimentController.handle);
+
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 import "reflect-metadata";
 import "./database";
